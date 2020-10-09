8b1572fae1eee377cfa0db6a3cc4e90168366ad6
vistritpandey <vistritpandey@gmail.com>
2020-10-09 17:07:33 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,23 @@ import './Home.css'
 function Home() {
     return (
         <div className='home'>
-            
+            <h1>This is home page</h1>
+            <div className='home__header'>
+                <div className='home__headerleft'>
+                    {/*Link*/}
+                    {/*Link*/}
+                </div>
+                <div className='home__headerright'>
+                    {/*Link*/}
+                    {/*Link*/}
+                    {/*Icon*/}
+                    {/*Avatar*/}
+                </div>
+            </div>
+
+            <div className='home__body'>
+
+            </div>
         </div>
     )
 }
