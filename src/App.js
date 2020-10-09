a7bd22f4f177cfd32e62cc305d5a12d4737acd4b
vistritpandey <vistritpandey@gmail.com>
2020-10-09 17:17:41 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> Well Hello There </h1>
+    <Router>
+
+    <switch>
+      <Route path="/search">
+        <h1>This is the search page</h1>
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </switch>
 
       {/*home page */}
       <Home />
 
       {/*search page*/}
+    </Router>
     </div>
   );
 }
