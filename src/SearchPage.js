e5a41d256915ea7e6697a1e64700e4ee3052b4a1
vistritpandey <vistritpandey@gmail.com>
2020-10-13 21:36:01 +0530

    bug fixed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './SearchPage.css'
 import { useStateValue } from './StateProvider'
+import useGoogleSearch from './useGoogleSearch';
 
 function SearchPage() {
     const[{ term }, dispatch] = useStateValue();
-    
+    const { data } = useGoogleSearch(term);
+
+    console.log(data)
     return (
         <div className='searchpage'>
            <div className='searchpage__header'>
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -9,7 +9,7 @@ const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
     useEffect (() => {
         const fetchData = async()=> {
-            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx={CONTEXT_KEY}&q=${term}`
+            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
             )
             .then(response => response.json())
             .then(result => {
