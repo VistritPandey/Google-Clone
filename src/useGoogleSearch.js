bb0212e83eed2691856a430eab5404b9c671c025
vistritpandey <vistritpandey@gmail.com>
2020-10-13 21:28:29 +0530

    m

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -5,9 +5,23 @@ import API_KEY from "./keys"
 const CONTEXT_KEY = "920d8f4c9be4ad7d4";
 
 
-const useGoogleSearch = () => {
+const useGoogleSearch = (term) => {
+    const [data, setData] = useState(null);
+    useEffect (() => {
+        const fetchData = async()=> {
+            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx={CONTEXT_KEY}&q=${term}`
+            )
+            .then(response => response.json())
+            .then(result => {
+                setData(result)
+            })
+        }
+
+        fetchData();
+    }, [term])
+
+    return { data }
     
-    
-}
+};
 
 export default useGoogleSearch
