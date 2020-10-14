8df7586b23106250b8c7d80a06be1f7d6fa3e6be
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-10-14 08:36:46 -0400

    Removed the key

--- src/keys.js
+++ src/keys.js
@@ -1,3 +1,3 @@
-export const API_KEY ="AIzaSyAYdSUtz6ir7LVwMLi5FPIczTZr_526aaM";
+export const API_KEY ="please use your own, because I have a limit of only 100 free requests per day :)";
 
 export default API_KEY;
